7c6afa8ee548b1ba1cd80977e6a6745da677dcba
DanilAlexandrovich <vosmerka97@mail.ru>
2023-06-11 18:05:02 +0500

    Initial commit

--- src/example4.java
+++ src/example4.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class example4 {
+
+import java.time.LocalTime;
+
+public class example4 {
+    public static void main(String[] args) {
+        int n = 10;
+        Thread[] th = new Thread[n];
+
+        for(int i=0; i<n; i++){
+            th[i] = new Thread(() -> {
+                System.out.println(Thread.currentThread().getName() + ": " + LocalTime.now());
+            });
+        }
+
+        for(int i=0; i<n; i++){
+            th[i].setName("Thread #" + (i+1));
+            th[i].start();
+        }
+    }
 }
+// Напишите программу, которая создает 10 потоков и каждый поток выводит на экран свой номер.
